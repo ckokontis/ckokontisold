0d018d9dd9b7678830c4e707bbb465158d3db308
ckokontis <ClaireKokontis@Claires-MacBook-Air-2.local>
2014-11-13 20:04:52 -0800

    font arvo

---
+++ Code/test.cpp
@@ -0,0 +1,17 @@
+//
+//  test.cpp
+//  
+//
+//  Created by Claire Kokontis on 11/9/14.
+//
+//
+
+#include "test.h" 
+#include <iostream>
+using namespace std;
+include main()
+{
+    
+    cout <<"hello world\n"<<endl;
+    return 0;
+}
---
+++ Code/test.h
@@ -0,0 +1,14 @@
+//
+//  test.h
+//  
+//
+//  Created by Claire Kokontis on 11/9/14.
+//
+//
+
+#ifndef ____test__
+#define ____test__
+
+#include <stdio.h> 
+#endif /* defined(____test__) */
+
